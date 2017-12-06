cbe18d5c359614fec745b43f73e60f491015cc80
tomekbuszman <tomasz.buszman0309@gmail.com>
2017-12-06 22:01:16 +0100

    stop time when finish

--- game/zegar.js
+++ game/zegar.js
@@ -1,7 +1,11 @@
+const ONE_SECOND = 1000;
+
 class Zegarek {
     constructor (){
         this.limitTime = null;
+        this.currentTime = 0;
         this.$clock = document.querySelector('h1');
+        this.zegarek = null;
     }
 
     render(string) {
@@ -10,18 +14,33 @@ class Zegarek {
 
     start(formatedTime) {
         this.limitTime = Zegarek.parseSeconds(formatedTime);
-    }
+        this.zegarek = setInterval (() => {
+            this.currentTime += ONE_SECOND;
+             let diff = this.limitTime - this.currentTime;
+             this.render(diff);
 
+                if(this.isFinished()){
+                    this.stop();
+                }
+            }, ONE_SECOND);
+    }
+        stop(){
+            clearInterval(this.zegarek)
+            }
+
+        isFinished () {
+            return this.currentTime - this.limitTime;
+        }
     static parseSeconds(time){
         let[minutes, seconds]=time.split(':').map(Numbers);
-        console.log(minutes, seconds);
+        return minutes * 60 * ONE_SECOND + seconds * ONE_SECOND;
         return 4000;
     }
 }
 
 function setup (){
     let zegarek1 = new Zegarek();
-    zegarek1.start();
+    zegarek1.start('10:00');
 }
 
 window.addEventListener('DOMcontentLoader', setup);
