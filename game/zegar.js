03476a428d96a8c3393edb588268fa865b4cf0ca
tomekbuszman <tomasz.buszman0309@gmail.com>
2017-12-06 22:23:42 +0100

    working clock

--- game/zegar.js
+++ game/zegar.js
@@ -9,21 +9,29 @@ class Zegarek {
     }
 
     render(string) {
+        let time = Zegarek.formatedTime();
         this.$clock.textContent = string;
     }
 
     start(formatedTime) {
         this.limitTime = Zegarek.parseSeconds(formatedTime);
+        this.update();
         this.zegarek = setInterval (() => {
             this.currentTime += ONE_SECOND;
-             let diff = this.limitTime - this.currentTime;
-             this.render(diff);
-
+               this.update();
                 if(this.isFinished()){
                     this.stop();
                 }
             }, ONE_SECOND);
     }
+        update(){
+            let remain = this.getRemainingTime();
+            let time = Zegarek.formatedTime(remain);
+            this.render(time);
+        }
+        getRemainingTime () {
+            return this.limitTime - this.currentTime;
+        }
         stop(){
             clearInterval(this.zegarek)
             }
@@ -35,6 +43,14 @@ class Zegarek {
         let[minutes, seconds]=time.split(':').map(Numbers);
         return minutes * 60 * ONE_SECOND + seconds * ONE_SECOND;
         return 4000;
+    }
+
+    static formatedTime(miliseconds){
+        let minutes = Math.floor(miliseconds/ONE_SECOND/60);
+        let seconds = miliseconds/ONE_SECOND%60;
+        minutes = String(minutes).padStart(2,'0');
+        seconds = String(seconds).padStart(2,'0');
+        return '${minutes}:${seconds}';
     }
 }
 
