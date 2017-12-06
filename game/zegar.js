1274f0dd7c3fb141479742246282fe6ade279ada
tomekbuszman <tomasz.buszman0309@gmail.com>
2017-12-06 21:42:20 +0100

    minutes and seconds

--- game/zegar.js
+++ game/zegar.js
@@ -1,13 +1,22 @@
 class Zegarek {
     constructor (){
+        this.limitTime = null;
         this.$clock = document.querySelector('h1');
     }
 
     render(string) {
         this.$clock.textContent = string;
-
     }
 
+    start(formatedTime) {
+        this.limitTime = Zegarek.parseSeconds(formatedTime);
+    }
+
+    static parseSeconds(time){
+        let[minutes, seconds]=time.split(':').map(Numbers);
+        console.log(minutes, seconds);
+        return 4000;
+    }
 }
 
 function setup (){
